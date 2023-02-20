36f7a14784f6e14e0692b4e61c8ebe6e8c9a7cd0
Deiximar Virriel <deiximar@gmail.com>
2023-02-20 23:07:10 +0100

    Fix format of html, js and (s)css files

--- src/wwwroot/js/letterpic.js
+++ src/wwwroot/js/letterpic.js
@@ -1,11 +1,8 @@
-
 $(document).ready(function () {
-
-$(".demo").letterpic({
+  $(".demo").letterpic({
     colors: ["#3333CC", "#F6F6F8", "#222", "#333"],
     font: "Tahoma",
     fontColor: "#FFFFFF",
-    fontSize: 0.3
+    fontSize: 0.3,
   });
-
 });
